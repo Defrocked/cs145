294e08730883821814fd7d0aa72035fe49d050b8
Mikey Beauchamp <defrocked@hotmail.com>
2012-03-27 19:14:08 -0500

    initial commit

---
+++ src/hw2/KeyholeIO.java
@@ -0,0 +1,34 @@
+package hw2;
+
+import java.io.PrintWriter;
+
+public class KeyholeIO {
+	
+	public static void openKML(PrintWriter pw){
+//		<?xml version="1.0" encoding="UTF-8"?>
+//		<kml xmlns="http://www.opengis.net/kml/2.2">
+//		<Document>
+//		<Style id="seethrough"><PolyStyle><color>7fffffff</color></PolyStyle></Style>
+		
+	}
+	
+	public static void closeKML(PrintWriter pw){
+		
+	}
+	
+	public static void writePin(PrintWriter pw, String placeName, String placeDescription, String latlong){
+		
+	}
+	
+	public static void writeLine(PrintWriter pw, String placeName, String placeDescription, String latlong){
+		
+	}
+	
+	public static void writeRegion(PrintWriter pw, String placeName, String placeDescription, String latlong){
+		
+	}
+	
+	public static void writePlacemark(PrintWriter pw, String placeName, String placeDescription, String latlong){
+		
+	}
+}
---
+++ src/hw2/WasHere.java
@@ -0,0 +1,17 @@
+package hw2;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+
+public class WasHere {
+
+	public static void main(String[] args) throws FileNotFoundException {
+		//PrintWriter pw = new PrintWriter(System.out, true);
+		
+		convert("src/hw2/washere eg.txt", "src/hw2/kmlthing.kml");
+	}
+
+	public static void convert(String filePath, String writeTo){
+
+	}
+
+}

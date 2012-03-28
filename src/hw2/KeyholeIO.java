d8db2a39c9abff75bd45c8ebeb3174791bec56ea
Mikey Beauchamp <defrocked@hotmail.com>
2012-03-27 20:30:45 -0500

    KeyholeIO done

--- src/hw2/KeyholeIO.java
+++ src/hw2/KeyholeIO.java
@@ -5,8 +5,8 @@ import java.io.PrintWriter;
 public class KeyholeIO {
 
 	public static void openKML(PrintWriter pw) {
-		//writes the beginning KML stuff
-		
+		// writes the beginning KML stuff
+
 		// <?xml version="1.0" encoding="UTF-8"?>
 		// <kml xmlns="http://www.opengis.net/kml/2.2">
 		// <Document>
@@ -19,25 +19,26 @@ public class KeyholeIO {
 	}
 
 	public static void closeKML(PrintWriter pw) {
-		//writes the ending KML stuff
-		
+		// writes the ending KML stuff
+
 		// </Document>
 		// </kml>
 		pw.println("</Document>");
 		pw.println("</kml>");
 	}
 
-	public static void writePin(PrintWriter pw, String placeName, String placeDescription, String latlong){
-		//writes for a pin KML
-		
-//		<!-- A pin placemark -->
-//		<Placemark>
-//		<name>NAME</name>
-//		<description>DESCRIPTION</description>
-//		<Point>
-//		<coordinates>LAT0,LON0</coordinates>
-//		</Point>
-//		</Placemark>
+	public static void writePin(PrintWriter pw, String placeName,
+			String placeDescription, String latlong) {
+		// writes for a pin KML
+
+		// <!-- A pin placemark -->
+		// <Placemark>
+		// <name>NAME</name>
+		// <description>DESCRIPTION</description>
+		// <Point>
+		// <coordinates>LAT0,LON0</coordinates>
+		// </Point>
+		// </Placemark>
 		pw.println("<!-- A pin placemark -->");
 		pw.println("<Placemark>");
 		pw.println("<name>" + placeName + "</name>");
@@ -48,18 +49,19 @@ public class KeyholeIO {
 		pw.println("</Placement>");
 	}
 
-	public static void writeLine(PrintWriter pw, String placeName, String placeDescription, String latlong) {
-		//writes for a line KML
-		
-//		<!-- A line placemark -->
-//		<Placemark>
-//		<name>NAME</name>
-//		<description>DESCRIPTION</description>
-//		<LineString>
-//		<tessellate>1</tessellate>
-//		<coordinates>LAT0,LON0 LAT1,LON1 LAT2,LON2 ...</coordinates>
-//		</LineString>
-//		</Placemark>
+	public static void writeLine(PrintWriter pw, String placeName,
+			String placeDescription, String latlong) {
+		// writes for a line KML
+
+		// <!-- A line placemark -->
+		// <Placemark>
+		// <name>NAME</name>
+		// <description>DESCRIPTION</description>
+		// <LineString>
+		// <tessellate>1</tessellate>
+		// <coordinates>LAT0,LON0 LAT1,LON1 LAT2,LON2 ...</coordinates>
+		// </LineString>
+		// </Placemark>
 		pw.println("<!-- A line placemark -->");
 		pw.println("<Placemark>");
 		pw.println("<name>" + placeName + "</name>");
@@ -71,22 +73,23 @@ public class KeyholeIO {
 		pw.println("</Placement>");
 	}
 
-	public static void writeRegion(PrintWriter pw, String placeName, String placeDescription, String latlong) {
-		//writes for a region KML
-		
-//		<!-- A region placemark -->
-//		<Placemark>
-//		<name>NAME</name>
-//		<description>DESCRIPTION</description>
-//		<styleUrl>#seethrough</styleUrl>
-//		<Polygon>
-//		<outerBoundaryIs>
-//		<LinearRing>
-//		<coordinates>LAT0,LON0 LAT1,LON1 LAT2,LON2 ...</coordinates>
-//		</LinearRing>
-//		</outerBoundaryIs>
-//		</Polygon>
-//		</Placemark>
+	public static void writeRegion(PrintWriter pw, String placeName,
+			String placeDescription, String latlong) {
+		// writes for a region KML
+
+		// <!-- A region placemark -->
+		// <Placemark>
+		// <name>NAME</name>
+		// <description>DESCRIPTION</description>
+		// <styleUrl>#seethrough</styleUrl>
+		// <Polygon>
+		// <outerBoundaryIs>
+		// <LinearRing>
+		// <coordinates>LAT0,LON0 LAT1,LON1 LAT2,LON2 ...</coordinates>
+		// </LinearRing>
+		// </outerBoundaryIs>
+		// </Polygon>
+		// </Placemark>
 		pw.println("<!-- A region placemark -->");
 		pw.println("<Placemark>");
 		pw.println("<name>" + placeName + "</name>");
@@ -103,11 +106,28 @@ public class KeyholeIO {
 	}
 
 	public static void writePlacemark(PrintWriter pw, String placeName, String placeDescription, String latlong) {
+		//counts the number of commas
+		String s = latlong;
+		int charCount = s.replaceAll("[^,]", "").length();
+		//checks to see if the first and last coord is the same
+		String[] t = s.split(" ");
+		boolean comparison = false;
+		if (t[0].equals(t[t.length-1])) {
+			comparison = true;
+		}
+		//makes a new string that cuts off the last coord if it was the same
+		String newString = "";
+		if (comparison) {
+			newString = t[0];
+			for (int i = 1; i <= t.length-2; i++) {
+				newString += " " + t[i];
+			}
+		}
 		//writes stuff using the other methods
-		if(1){
+		if(charCount == 1){
 			writePin(pw, placeName, placeDescription, latlong);
-		}else if(2){
-			writeRegion(pw, placeName, placeDescription, latlong);
+		}else if(comparison){
+			writeRegion(pw, placeName, placeDescription, newString);
 		}else{
 			writeLine(pw, placeName, placeDescription, latlong);
 		}
